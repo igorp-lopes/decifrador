28738fea7b35efdc542202739b0739a60c3ec59b
Igor <igorp.lopes16@gmail.com>
2020-06-28 00:17:35 -0300

    Criado o algoritmo para iterar pela palavra

--- src/main.c
+++ src/main.c
@@ -10,13 +10,28 @@
 char buffer_entrada[TAM_BUFFER];
 char buffer_saida[TAM_BUFFER];
 
-int main() {
-  /* Leitura da entrada */
+int main()
+{
+  const char palChave[] = "SENHASECRETA";
+  // Iteradores
+  int i = 0;
+  int j = 0;
+
+  // Leitura da entrada */
   fgets(buffer_entrada, TAM_BUFFER, stdin);
 
+  // Iteramos pelo texto de entrada até o seu fim
+  for (i = 0; (i < TAM_BUFFER) && (buffer_entrada[i] != '\0') && (buffer_entrada[i] != '\n'); i++)
+  {
 
+    buffer_saida[i] = decifrador(buffer_entrada[i], palChave[j]); // Obtemos o caracter descriptografado
+    j++; // Incrementamos o iterador para selecionarmos o próximo caracter da palavra chave
+    if (palChave[j] =='\0') j=0; // Se chegamos ao final da palavra chave, retornamos para o seu início
+  }
 
-  /* Escrever saida na tela */
+  buffer_saida[i]='\0'; // Inserimos o caracter de fim da string
+
+  // Escrever saida na tela */
   printf("%s\n", buffer_saida);
 
   return 0;
